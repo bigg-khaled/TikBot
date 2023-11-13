11bbf64e0d0a9d7fd0fa0554d00b85138b092b92
EyadTamer999 <eyadhassan2002@gmail.com>
2023-11-13 19:34:51 +0200

    wallhi ma ba2et fahem

--- TikTokUploader.py
+++ TikTokUploader.py
@@ -14,13 +14,6 @@ cookies_file = "cookies.txt"
 # Get a list of video files in the folder
 video_files = [f for f in os.listdir(video_folder) if f.endswith(".mp4")]
 
-# Schedule uploads for 10 minutes from now
-# scheduled_time = datetime.datetime.now() + datetime.timedelta(minutes=20)
-
-# upload_video(
-#     "BottomVideo/bg.mp4", description="bg", cookies=cookies_file, browser="edge"
-# )
-
 # Iterate through each video file and upload using Chrome in headless mode
 for video_file in video_files:
     video_path = os.path.join(video_folder, video_file)
@@ -38,12 +31,7 @@ for video_file in video_files:
         description=description,
         cookies=cookies_file,
         browser="edge",
-        # headless=True,
-        # schedule=scheduled_time,
     )
-
-    # Increment the scheduled time for the next upload
-    # scheduled_time += datetime.timedelta(minutes=20)
 
     # Delete the file after successful upload
     # os.remove(video_path)
