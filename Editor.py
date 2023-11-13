36d15087e756bb60c93ca89034db331c84825d72
EyadTamer999 <eyadhassan2002@gmail.com>
2023-11-13 17:40:41 +0200

    8ayert kda shwytt 7agat fel lazeez

--- Editor.py
+++ Editor.py
@@ -1,6 +1,7 @@
 import os
 from moviepy.editor import VideoFileClip, clips_array
 from moviepy.video import fx as vfx
+import random
 
 # Input folders containing video clips
 input_folder1 = "TopVideo"
@@ -16,13 +17,21 @@ video_files1 = [
     f for f in os.listdir(input_folder1) if f.endswith((".mp4", ".avi", ".mkv"))
 ]
 
+# Get the first video file from the second input folder
+video_files2 = [
+    f for f in os.listdir(input_folder2) if f.endswith((".mp4", ".avi", ".mkv"))
+]
+
 # Loop over each video file in the first folder
 for video_file1 in video_files1:
+    # random video file from the second folder
+    video_file2 = random.choice(video_files2)
+
     # Get the full path of the input video file in the first folder
     input_video_path1 = os.path.join(input_folder1, video_file1)
 
     # Get the user input for the number of parts to divide the video into
-    p = int(input(f"How many parts would you like {video_file1} to be divided into? "))
+    p = 2  # int(input(f"How many parts would you like {video_file1} to be divided into? "))
 
     # Get the duration of the input video file in the first folder
     length1 = VideoFileClip(input_video_path1).duration
@@ -30,60 +39,52 @@ for video_file1 in video_files1:
     # Create the video clip for the current input file in the first folder
     clip1 = VideoFileClip(input_video_path1).subclip(0, 0 + length1)
 
-    # Get the list of all video files in the second input folder
-    video_files2 = [
-        f for f in os.listdir(input_folder2) if f.endswith((".mp4", ".avi", ".mkv"))
-    ]
+    # Get the full path of the input video file in the second folder
+    input_video_path2 = os.path.join(input_folder2, video_file2)
 
-    # Loop over each video file in the second folder
-    for video_file2 in video_files2:
-        # Get the full path of the input video file in the second folder
-        input_video_path2 = os.path.join(input_folder2, video_file2)
+    # Get the duration of the input video file in the second folder
+    length2 = VideoFileClip(input_video_path2).duration
 
-        # Get the duration of the input video file in the second folder
-        length2 = VideoFileClip(input_video_path2).duration
+    # Create the video clip for the first file in the second folder
+    clip2 = VideoFileClip(input_video_path2).subclip(0, 0 + length2)
 
-        # Create the video clip for the current input file in the second folder
-        clip2 = VideoFileClip(input_video_path2).subclip(0, 0 + length2)
+    # make clip2 loop with the duration of clip1
+    clip2 = vfx.all.loop(clip2, duration=length1)
 
-        # get the number of times clip2 should be repeated to match the length of clip1
-        n = int(length1 / length2) + 1
+    # Mute the audio of clip2
+    clip2 = clip2.set_audio(None)
 
-        # make clip2 loop with duration of clip1
-        clip2 = vfx.all.loop(clip2, duration=length1)
+    # Combine clips1 and clip2 into a single video array
+    combined = clips_array([[clip1], [clip2]])
 
-        # Mute the audio of clip2
-        clip2 = clip2.set_audio(None)
+    # Write the combined clips to a temporary video file
+    temp_output_path = os.path.join(output_folder, "temp.mp4")
+    combined.write_videofile(temp_output_path)
 
-        # Combine clips1 and clip2 into a single video array
-        combined = clips_array([[clip1], [clip2]])
+    current_duration = VideoFileClip(temp_output_path).duration
+    single_duration = current_duration / p
 
-        # Write the combined clips to a temporary video file
-        temp_output_path = os.path.join(output_folder, "temp.mp4")
-        combined.write_videofile(temp_output_path)
+    # Loop to create separate video files for each part
+    for part_number in range(1, p + 1):
+        clip = VideoFileClip(temp_output_path).subclip(
+            current_duration - single_duration, current_duration
+        )
 
-        current_duration = VideoFileClip(temp_output_path).duration
-        single_duration = current_duration / p
+        current_video = f"{video_file1.replace('.', f'_Part{part_number}.')}"
+        output_path = os.path.join(output_folder, current_video)
 
-        # Loop to create separate video files for each part
-        for part_number in range(1, p + 1):
-            clip = VideoFileClip(temp_output_path).subclip(
-                current_duration - single_duration, current_duration
-            )
+        # Write the subclip to the output file, specifying codec and audio settings
+        clip.to_videofile(
+            output_path,
+            codec="libx264",
+            temp_audiofile="temp-audio.m4a",
+            remove_temp=True,
+            audio_codec="aac",
+        )
 
-            current_video = f"{video_file1.replace('.', f'_Part{part_number}.')}"
-            output_path = os.path.join(output_folder, current_video)
+        print(f"Result saved to: {output_path}")
+        print("-----------------###-----------------")
 
-            # Write the subclip to the output file, specifying codec and audio settings
-            clip.to_videofile(
-                output_path,
-                codec="libx264",
-                temp_audiofile="temp-audio.m4a",
-                remove_temp=True,
-                audio_codec="aac",
-            )
-
-            print(f"Result saved to: {output_path}")
-            print("-----------------###-----------------")
-
-            current_duration -= single_duration
+        current_duration -= single_duration
+# delete the temporary file
+os.remove(temp_output_path)
--- TikTokUploader.py
+++ TikTokUploader.py
@@ -1,15 +1,45 @@
 from tiktok_uploader.upload import upload_video
 from tiktok_uploader.auth import AuthBackend
+import os
+import datetime
 
-# Replace 'video.mp4', 'description', and 'cookies.txt' with your actual values
-video_path = "Videos/bg.mp4"
-description = "This is my video description"
+# Replace 'ResultClips', 'cookies.txt' with your actual values
+video_folder = "ResultClips"
 cookies_file = "cookies.txt"
 
-# Upload the video
+# Get a list of video files in the folder
+video_files = [f for f in os.listdir(video_folder) if f.endswith(".mp4")]
+
+# Schedule uploads for 10 minutes from now
+scheduled_time = datetime.datetime.now() + datetime.timedelta(minutes=20)
+
 upload_video(
-    "bg.mp4",
-    description="this is my description",
-    cookies="cookies.txt",
-    browser="edge",
+    "BottomVideo/bg.mp4", description="bg", cookies=cookies_file, browser="edge"
 )
+
+# # Iterate through each video file and upload using Chrome in headless mode
+# for video_file in video_files:
+#     video_path = os.path.join(video_folder, video_file)
+
+#     # Get the description from the video file name
+#     description = (
+#         video_file.replace(".mp4", "").replace("_", " ")
+#         + "\n "
+#         + "#fyp #film #movie #tiktok #foryou #feelinggood"
+#     )
+
+#     # Upload the video using Chrome in headless mode and schedule it
+#     upload_video(
+#         video_path,
+#         description=description,
+#         cookies=cookies_file,
+#         browser="edge",
+#         # headless=True,
+#         schedule=scheduled_time,
+#     )
+
+#     # Increment the scheduled time for the next upload
+#     scheduled_time += datetime.timedelta(minutes=20)
+
+#     # Delete the file after successful upload
+#     # os.remove(video_path)
